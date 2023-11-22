75c827a0e1891602121e548d5af25126345d0122
Ash <126400612+AishSweety@users.noreply.github.com>
2023-11-22 21:29:47 +0100

    Noisy_Quantum_circuit.py

--- Noisy_Quantum_circuit.py
+++ Noisy_Quantum_circuit.py
@@ -1,30 +1,26 @@
 import pennylane as qml
 import numpy as np
 
-N_QUBITS = 4
+N_QUBITS = 2
 NOISE_STRENGTH = 0.05
 NUM_LAYERS = 3
-NUM_STEPS = 200
-STEP_SIZE = 0.05
+NUM_STEPS = 500  # Increased number of steps
+STEP_SIZE = 0.1  # Adjusted step size
 
 dev = qml.device("default.mixed", wires=N_QUBITS, shots=1000)
 
 @qml.qnode(dev)
 def noisy_circuit(var_params, x=None):
-    # Encoding using a loop for more qubits
     for i, xi in enumerate(x):
         qml.RX(xi, wires=i)
         qml.DepolarizingChannel(NOISE_STRENGTH, wires=i)
 
-    # More layers in the variational part
     for _ in range(NUM_LAYERS):
         for i in range(N_QUBITS):
-            qml.RX(var_params[i], wires=i)
-            qml.RY(var_params[i + N_QUBITS], wires=i)
+            qml.Rot(var_params[i], var_params[i + N_QUBITS], var_params[i + 2 * N_QUBITS], wires=i)
         for i in range(N_QUBITS - 1):
             qml.CNOT(wires=[i, i + 1])
 
-    # Measurement
     return [qml.expval(qml.PauliZ(i)) for i in range(N_QUBITS)]
 
 def noisy_cost(var_params, x=None, y=None):
@@ -32,18 +28,30 @@ def noisy_cost(var_params, x=None, y=None):
     return np.sum((predictions - y) ** 2)
 
 def optimize_with_noise():
-    var_params = qml.numpy.random.rand(2 * N_QUBITS)
+    # Adjusting the initial parameters with a smaller range
+    var_params = qml.numpy.random.uniform(low=-0.1, high=0.1, size=3 * N_QUBITS)
     true_thetas = np.pi * np.random.rand(N_QUBITS)
-    observed_values = np.array(noisy_circuit(var_params, x=true_thetas))
 
-    # Using the Adam optimizer
+    initial_params = var_params.copy()
+
+    # Using the Adam optimizer with an increased step size
     opt = qml.AdamOptimizer(stepsize=STEP_SIZE)
 
-    for _ in range(NUM_STEPS):
-        var_params = opt.step(noisy_cost, var_params, x=true_thetas, y=observed_values)
+    for i in range(NUM_STEPS):
+        # Evaluate the initial circuit with true parameters
+        observed_values = np.array(noisy_circuit(var_params, x=true_thetas))
 
+        # Pass true_thetas as the target values for the optimization
+        var_params = opt.step(noisy_cost, var_params, x=true_thetas, y=true_thetas)
+
+        # Print the cost every 50 steps
+        if i % 50 == 0:
+            print(f"Step {i+1}/{NUM_STEPS} - Cost: {noisy_cost(var_params, x=true_thetas, y=true_thetas)}")
+
+    print("\nOptimization complete.")
+    print("Initial parameters:", initial_params)
     print("Optimized parameters with noise:", var_params)
-    print("Final cost with noise:", noisy_cost(var_params, x=true_thetas, y=observed_values))
+    print("Final cost with noise:", noisy_cost(var_params, x=true_thetas, y=true_thetas))
 
 if __name__ == "__main__":
     optimize_with_noise()
